c387b51c7804b984c842d66a99d088c1ee813e65
Dhruv Prantijkar <93885016+dhruvprantijkar@users.noreply.github.com>
2024-01-12 09:40:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-const accesskey = "aEfn8Ike4XkMec E4Sir8r2Y20cv6_mlvfODHH2RCacs"
+const accesskey = "B8s3XfJPyjibaEutLah-sHxlowuk0kGPJV0nhSyaPGk"
 
 const formE1 = document.querySelector("form")
 const inputE1 = document.getElementById("Search-input")
-const SearchResults = document.querySelector(".Search-results")
+const Searchresults = document.querySelector(".Search-results")
 const Showmore = document.getElementById("Show-more-button")
 
-let inputData=""
+let inputData="";
 let page=1;
 
 async function Searchimages(){
-    inputData = inputE1.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`
-    console.log("Helllo");
-    const Response = await fetch(url)
-    const data = await Response.json()
+  inputData = inputE1.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`;
 
-    const results = data.results
+  const Response = await fetch(url);
+  const data = await Response.json();
 
-    if (page===1){
-        SearchResults.innerHTML = ""
-    }
-    results.map((result)=>{
-        const imageWrapper = document.createElement('div')
-        imageWrapper.classlist.add("search-result")
-        const image = document.createElement('img')
-    })
+  const results = data.results;
+
+  if (page===1){
+    Searchresults.innerHTML = "";
+  }
+
+  results.map((result)=>{
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add("Search-result");
+    const image = document.createElement('img');
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.target="_blank";
+    imageLink.href = result.links.html;
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    Searchresults.appendChild(imageWrapper);
+  });
+
+  page++;
+  if(page > 1){
+   Showmore.style.display = "block";
+  }
 }
+
+formE1.addEventListener("submit", (event) => {
+  event.preventDefault();    
+  page = 1;
+  Searchimages();
+});
+
+Showmore.addEventListener("click",() => {
+  Searchimages();
+});
